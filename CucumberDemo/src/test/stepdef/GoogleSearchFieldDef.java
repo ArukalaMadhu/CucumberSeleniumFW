7c5f8eeb1445a5e8e76779090d2509aec507bfe7
ArukalaMadhu <arukalamadhu@gmail.com>
2021-01-01 23:14:49 +0530

    test1

--- CucumberDemo/src/test/stepdef/GoogleSearchFieldDef.java
+++ CucumberDemo/src/test/stepdef/GoogleSearchFieldDef.java
@@ -48,6 +48,7 @@ public class GoogleSearchFieldDef extends SeleniumActions {
 //		quitDriver();
 //	}
 		
+	
 	@Given("^Open google \"(.*)\"$") //\"([^\"]*)\"
 	public void open_google_URL(String URL) {
 	   openURL(URL);
